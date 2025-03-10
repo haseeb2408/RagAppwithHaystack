96f51257cd5a14146dd783cd14e036fa7e87afc9
Haseeb Ahmed <ahmed0499280@gmail.com>
2025-03-10 11:15:48 +0500

    folder structure updated

---
+++ RagApp/ingestion.py
@@ -0,0 +1,11 @@
+from haystack import Pipeline
+from haystack.components.writers import DocumentWriter
+from haystack.components.preprocessors import DocumentSplitter
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.components.converters import PyPDFToDocument
+from haystack_integrations.document_stores.pinecone import PineconeDocumentStore
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
---
+++ RagApp/retrievalandgeneration.py
@@ -0,0 +1,8 @@
+from haystack.utils import Secret
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.builders import PromptBuilder
+from haystack_integrations.components.retrievers.pinecone import PineconeEmbeddingRetriever
+from haystack.components.generators import HuggingFaceTGIGenerator
+from haystack import Pipeline
+import os
+from dotenv import load_dotenv
